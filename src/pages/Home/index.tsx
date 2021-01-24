a770255e1d350359a50bf57d063148fa0f32986c
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2021-01-23 23:30:48 -0300

    feat: Iniciando Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import Theme from '../../styles/default';
@@ -7,9 +8,45 @@ import { Container } from './styles';
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      <ThemeProvider theme={Theme} />
-    </Container>
+    <ThemeProvider theme={Theme}>
+      <Container>
+        <h1>Bem vindo a Pokémon Marketplace!!!</h1>
+        <p>Escolha qual loja deseja seguir inicialmente.</p>
+
+        <main>
+          <figure>
+            <Link to="/dark">
+              <img
+                src="https://pixelmonmod.com/w/images/f/f8/DarkType.png"
+                alt="Dark Type"
+              />
+
+              <figcaption>Dark Store</figcaption>
+            </Link>
+          </figure>
+          <figure>
+            <Link to="/fire">
+              <img
+                src="https://pixelmonmod.com/w/images/7/79/FireType.png"
+                alt="Fire Type"
+              />
+
+              <figcaption>Fire Store</figcaption>
+            </Link>
+          </figure>
+          <figure>
+            <Link to="/ice">
+              <img
+                src="https://pixelmonmod.com/w/images/1/10/IceType.png"
+                alt="Ice Type"
+              />
+
+              <figcaption>Ice Store</figcaption>
+            </Link>
+          </figure>
+        </main>
+      </Container>
+    </ThemeProvider>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,4 +6,70 @@ export const Container = styled.div`
 
   display: flex;
   flex-direction: column;
+  align-items: center;
+
+  /* background-color: ${(props) => props.theme.primary}; */
+
+  padding: 40px;
+  color: #000000;
+
+  h1 {
+    margin: 20px 0;
+  }
+
+  p {
+    font-weight: 400;
+    margin-bottom: 40px;
+  }
+
+  main {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    width: 100%;
+
+    figure {
+      width: 200px;
+      height: 200px;
+      /* background-color: ${(props) => props.theme.secondary}; */
+      border: 1px solid #a8a8b3;
+      border-radius: 4px;
+      padding: 5px;
+      margin: 10px;
+      border-radius: 12px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-transform: capitalize;
+
+      box-shadow: 1px 1px 7px 2px #a8a8b3;
+      transition: transform 0.2s;
+
+      :hover {
+        transform: scale(1.1);
+        /* background-color: ${(props) => props.theme.secondaryLight}; */
+        box-shadow: 0px 0px 4px 0px ${(props) => props.theme.primaryDark};
+      }
+      a {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        text-decoration: none;
+
+        figcaption {
+          margin-left: 5px;
+          font-size: 22px;
+          font-weight: 700;
+          margin-top: 10px;
+          text-transform: capitalize;
+        }
+      }
+    }
+  }
 `;
